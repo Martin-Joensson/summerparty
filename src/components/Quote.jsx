61fc697ca5149b0e7e093608ac0ecd316488074b
Martin Jönsson <martin.t.joensson@gmail.com>
2026-06-02 15:47:47 +0200

    Consolidated text-sizing, updated title Text

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export const Quote = () => {
   return (
     <div className="relative max-w-2xl mx-auto text-center my-20 px-4">
-      <h2 className="relative text-green-accent -rotate-2 text-center text-xl font-bold ">
+      <h2 className=" text-green-accent -rotate-2 text-center text-xl font-bold ">
         Samla dom du tycker om. Resten brukar lösa sig själv.
       </h2>
 
-      <div className="text-white mt-6 text-xl text-left">
+      <div className="text-white mt-6 text-left">
         <p>
           Vi fyller gården med sommarhäng, skratt, grill och roliga aktiviteter.
           Kom förbi och njut av en avslappnad eftermiddag och kväll tillsammans
